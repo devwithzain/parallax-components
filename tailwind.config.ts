fb2c5cda67c61ed5adc354a0f660c98d06270ade
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-14 18:29:13 +0500

    Modified

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,19 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./animation/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#f1f1f1",
+        secondry: "#212121"
       },
     },
+    fontFamily: {
+      Neglige: ["Neglige", "sans-serif"],
+      NeueMontreal: ["NeueMontreal", "sans-serif"],
+    }
   },
   plugins: [],
 };
